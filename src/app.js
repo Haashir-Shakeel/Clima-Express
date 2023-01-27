00031044fe528bb88fb7b8708443c19a28c07b6d
Haashir-Shakeel <david10james99@gmail.com>
2023-01-27 14:36:06 +0500

    initial setup of express, file setup

--- src/app.js
+++ src/app.js
@@ -1,22 +1,18 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
+const publicDirectory = path.join(__dirname, '../public')
 
-app.get('', (req, res)=>{
-    res.send('Hello express!')
-})
+app.use(express.static(publicDirectory))
 
-app.get('/help', (req,res)=>{
-    res.send('Help page')
-})
-
-app.get('/about', (req,res)=>{
-    res.send('About page')
-})
 
 app.get('/weather', (req,res)=>{
-    res.send('Weather page')
+    res.send({
+        location: 'Lahore',
+        forecast: " It is 14 degrees"
+    })
 })
 app.listen(3000, ()=>{
     console.log('Server is up on port 3000');
